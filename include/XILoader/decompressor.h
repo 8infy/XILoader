2147704504094a715cc2a45466bba2c708711687
8infy <99danilt@gmail.com>
2020-02-02 18:57:00 +0300

    Deflate decompressor pt.3 / fixed Huffman codes

--- include/XILoader/decompressor.h
+++ include/XILoader/decompressor.h
@@ -2,6 +2,7 @@
 
 #include "data_reader.h"
 
+#define FIXED_LITLEN 288
 #define MAX_LITLEN 286
 #define MAX_DIST   30
 #define MAX_BITS   15
@@ -44,6 +45,7 @@ namespace XIL {
                     break;
                 case 1:
                     // fixed huffman codes
+                    inflate_fixed(bit_stream, uncompressed_stream);
                     break;
                 case 2:
                     // dynamic huffman codes
@@ -120,7 +122,41 @@ namespace XIL {
             construct_tree(litlen_tree, lengths);
             construct_tree(distance_tree, lengths + hlit);
 
-            decompress_block(bit_stream, litlen_tree, distance_tree);
+            decompress_block(bit_stream, litlen_tree, distance_tree, uncompressed_stream);
+        }
+
+        static void inflate_fixed(ChunkedBitReader& bit_stream, ImageData::Container& uncompressed_stream)
+        {
+            static huffman_tree<FIXED_LITLEN> litlen_tree;
+            static huffman_tree<MAX_DIST>     distance_tree;
+
+            static bool constructed = false;
+
+            if (!constructed)
+            {
+                uint16_t symbol;
+                uint16_t lengths[FIXED_LITLEN];
+
+                for (symbol = 0; symbol < 144; symbol++)
+                    lengths[symbol] = 8;
+                for (; symbol < 256; symbol++)
+                    lengths[symbol] = 9;
+                for (; symbol < 280; symbol++)
+                    lengths[symbol] = 7;
+                for (; symbol < FIXED_LITLEN; symbol++)
+                    lengths[symbol] = 8;
+
+                construct_tree(litlen_tree, lengths);
+
+                for (symbol = 0; symbol < MAX_DIST; symbol++)
+                    lengths[symbol] = 5;
+
+                construct_tree(distance_tree, lengths);
+
+                constructed = true;
+            }
+
+            decompress_block(bit_stream, litlen_tree, distance_tree, uncompressed_stream);
         }
 
         template<typename HuffmanT>
@@ -182,8 +218,64 @@ namespace XIL {
         }
 
         template<typename HuffmanTL, typename HuffmanTD>
-        static void decompress_block(ChunkedBitReader& from, HuffmanTL& litlen_tree, HuffmanTD& distance_tree)
+        static void decompress_block(ChunkedBitReader& from, HuffmanTL& litlen_tree, HuffmanTD& distance_tree, ImageData::Container& uncompressed_stream)
         {
+            size_t initial_size = uncompressed_stream.size();
+            uncompressed_stream.reserve(initial_size + 8192);
+            size_t block_index = initial_size;
+
+            uint16_t symbol;
+            size_t length;
+            size_t distance;
+
+            static const uint16_t length_base[29] =
+            { 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 15, 17, 19, 23, 27, 31,
+              35, 43, 51, 59, 67, 83, 99, 115, 131, 163, 195, 227, 258 };
+
+            static const uint16_t length_extra[29] =
+            { 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2,
+              2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 0 };
+
+            static const uint16_t distance_base[30] =
+            { 1, 2, 3, 4, 5, 7, 9, 13, 17, 25, 33, 49, 65, 
+              97, 129, 193, 257, 385, 513, 769, 1025, 1537,
+              2049, 3073, 4097, 6145, 8193, 12289, 16385, 24577 };
+
+            static const uint16_t distance_extra[30] =
+            { 0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 4,
+              4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9,
+              10, 10, 11, 11, 12, 12, 13, 13 };
+
+            do {
+                symbol = decode_one(from, litlen_tree);
+
+                if (symbol < 256)
+                {
+                    uncompressed_stream.push_back(symbol);
+                    block_index++;
+                }
+                else if (symbol > 256)
+                {
+                    symbol -= 257;
+                    if (symbol >= 29)
+                        throw std::runtime_error("Length symbol is outside of [29] range");
+
+                    length = length_base[symbol] + from.get_bits(length_extra[symbol]);
+
+                    symbol = decode_one(from, distance_tree);
+
+                    distance = distance_base[symbol] + from.get_bits(distance_extra[symbol]);
+
+                    if (distance > block_index - initial_size)
+                        throw std::runtime_error("Distance is outside of the out block");
+
+                    while (length--)
+                    {
+                        uncompressed_stream.push_back(uncompressed_stream[block_index - distance]);
+                        block_index++;
+                    }
+                }
+            } while (symbol != 256);
 
         }
     };
